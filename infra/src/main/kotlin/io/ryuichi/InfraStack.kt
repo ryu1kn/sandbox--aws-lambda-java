ace29bfcb83f9c581b1a6d4ac25b6d3ea7bb3c6e
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2021-07-24 22:01:21 +1000

    Deploy lambda with sample java code

--- infra/src/main/kotlin/io/ryuichi/InfraStack.kt
+++ infra/src/main/kotlin/io/ryuichi/InfraStack.kt
@@ -1,26 +1,35 @@
 package io.ryuichi
 
-import software.amazon.awscdk.core.Construct
-import software.amazon.awscdk.core.Duration
-import software.amazon.awscdk.core.Stack
-import software.amazon.awscdk.core.StackProps
+import software.amazon.awscdk.core.*
 import software.amazon.awscdk.services.lambda.Code
+import software.amazon.awscdk.services.lambda.Function
+import software.amazon.awscdk.services.lambda.FunctionProps
 import software.amazon.awscdk.services.lambda.Runtime
-import software.amazon.awscdk.services.lambda.SingletonFunction
-import java.util.*
+import software.amazon.awscdk.services.s3.assets.AssetOptions
 
 class InfraStack(scope: Construct, id: String, props: StackProps?) : Stack(scope, id, props) {
     constructor(scope: Construct, id: String): this(scope, id, null)
 
     init {
-        SingletonFunction.Builder.create(this, "cdk-lambda-cron")
+        val packageInstructions = listOf("/bin/sh", "-c",
+            "./gradlew jar && cp build/libs/app.jar /asset-output/"
+        )
+
+        val builderOptions = BundlingOptions.builder()
+            .command(packageInstructions)
+            .image(Runtime.JAVA_11.bundlingImage)
+            .user("root")
+            .outputType(BundlingOutput.ARCHIVED)
+            .build()
+
+        Function(this, "cdk-lambda-cron", FunctionProps.builder()
             .functionName("cdk-lambda-cron")
             .description("Lambda which prints \"I'm running\"")
-            .code(Code.fromInline("def main(event, context):\n" + "    print(\"I'm running!\")\n"))
-            .handler("index.main")
+            .code(Code.fromAsset("../app/", AssetOptions.builder().bundling(builderOptions).build()))
+            .runtime(Runtime.JAVA_11)
+            .handler("example.Handler")
+            .memorySize(1024)
             .timeout(Duration.seconds(300))
-            .runtime(Runtime.PYTHON_2_7)
-            .uuid(UUID.randomUUID().toString())
-            .build();
+            .build())
     }
 }
