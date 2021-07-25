86390e293c14870024bf57707431eddfc142bb4c
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2021-07-25 21:44:15 +1000

    Updated lambda invocation to use a sample event

--- app/2-deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-ARTIFACT_BUCKET=$(cat bucket-name.txt)
-TEMPLATE=template.yml
-
-gradle build -i
-
-aws cloudformation package --template-file $TEMPLATE --s3-bucket $ARTIFACT_BUCKET --output-template-file out.yml
-aws cloudformation deploy --template-file out.yml --stack-name java-basic --capabilities CAPABILITY_NAMED_IAM
