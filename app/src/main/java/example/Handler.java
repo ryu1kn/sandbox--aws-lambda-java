5b29f179f95826e42a9a72147d3a8bcad5c575f3
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2021-07-25 12:05:08 +1000

    Let IntelliJ to cleanup files

--- app/src/main/java/example/Handler.java
+++ app/src/main/java/example/Handler.java
@@ -6,17 +6,16 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger;
 
 import java.util.Map;
 
-// Handler value: example.Handler
-public class Handler implements RequestHandler<Map<String,String>, String>{
-  @Override
-  public String handleRequest(Map<String,String> event, Context context) {
-    LambdaLogger logger = context.getLogger();
-    String response = "200 OK";
-    // log execution details
-    logger.log("ENVIRONMENT VARIABLES: " + System.getenv().toString());
-    logger.log("CONTEXT: " + context);
-    logger.log("EVENT: " + event.toString());
-    logger.log("EVENT TYPE: " + event.getClass());
-    return response;
-  }
+public class Handler implements RequestHandler<Map<String, String>, String> {
+    @Override
+    public String handleRequest(Map<String, String> event, Context context) {
+        LambdaLogger logger = context.getLogger();
+        String response = "200 OK";
+        // log execution details
+        logger.log("ENVIRONMENT VARIABLES: " + System.getenv().toString());
+        logger.log("CONTEXT: " + context);
+        logger.log("EVENT: " + event.toString());
+        logger.log("EVENT TYPE: " + event.getClass());
+        return response;
+    }
 }
--- app/src/test/java/example/TestLogger.java
+++ app/src/test/java/example/TestLogger.java
@@ -1,14 +1,17 @@
 package example;
+
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
 
 public class TestLogger implements LambdaLogger {
-  private static final Logger logger = LoggerFactory.getLogger(TestLogger.class);
-  public void log(String message){
-    logger.info(message);
-  }
-  public void log(byte[] message){
-    logger.info(new String(message));
-  }
+    private static final Logger logger = LoggerFactory.getLogger(TestLogger.class);
+
+    public void log(String message) {
+        logger.info(message);
+    }
+
+    public void log(byte[] message) {
+        logger.info(new String(message));
+    }
 }
