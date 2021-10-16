bc308f87c80bf993fc8b4b5f592e98124b52ddbd
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2021-10-16 13:20:50 +1100

    Put additional MDC

--- app/src/main/kotlin/io/ryuichi/Handler.kt
+++ app/src/main/kotlin/io/ryuichi/Handler.kt
@@ -3,17 +3,23 @@ package io.ryuichi
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
 import org.slf4j.LoggerFactory
+import org.slf4j.MDC
 
 class Handler : RequestHandler<Map<String, String>, String> {
 
     override fun handleRequest(event: Map<String, String>, context: Context): String {
-        val response = "200 OK"
+        try {
+            MDC.put("region", System.getenv("AWS_REGION"))
 
-        logger.info("ENVIRONMENT VARIABLES: " + System.getenv().toString())
-        logger.info("CONTEXT: $context")
-        logger.info("EVENT: $event")
-        logger.info("EVENT TYPE: " + event::class.java)
-        return response
+            logger.info("ENVIRONMENT VARIABLES: " + System.getenv().toString())
+            logger.info("CONTEXT: $context")
+            logger.info("EVENT: $event")
+            logger.info("EVENT TYPE: " + event::class.java)
+
+            return "200 OK"
+        } finally {
+            MDC.remove("region")
+        }
     }
 
     companion object {
